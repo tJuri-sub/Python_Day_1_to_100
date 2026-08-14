d5721db71e79b0353a975e364be8fbaefb1aa921
tjuri-sub <juricuber@gmail.com>
2026-08-14 14:36:07 +0800

    Day2 - variables

---
+++ Day2 - variable.py
@@ -0,0 +1,14 @@
+#Variables
+# must be start with letters and not numbers
+# variables are case sensitive age is different from Age
+# in python variables cannot be a python keyword
+# variable can only contain alphanumeric characters and underscores (A-z, 0-9, and _ )
+
+name = "Juri"
+age = 23
+height = 1.76
+programming_language = "Python"
+likes_robotics = True
+
+print(name, age, height, programming_language, likes_robotics)
+print("My name is", name, "I am", age, "years old, my height is", height, "meters, I like programming in", programming_language, "and it is", likes_robotics, "that I like robotics")
